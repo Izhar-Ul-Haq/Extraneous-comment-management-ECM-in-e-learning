91e04a171387bdc2f690ff9e063ab13b890100ec
Internee <Internee@LRO-SH-21.vu.edu.pk>
2022-12-20 10:53:43 +0500

    Hi this is the starting to ChatterBOT App

---
+++ helloWorld.py
@@ -0,0 +1,15 @@
+#Import Required Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# import plotly.express as pd
+import sklearn as sk
+import plotly as pt
+import seaborn as sns
+# import tensorflow as tf
+# import torch as th
+#Print Hello World
+print("Hello World!!!")
+print("We are trying hard to make a ChatterBOT and Open Source to Eveyone :)")
+#This is just starting and yet to continue . . .
+# print("TensorFlow version:", tf.__version__)
