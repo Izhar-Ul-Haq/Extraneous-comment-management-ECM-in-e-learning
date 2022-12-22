9e2d63976a5a9b3e40de66e8aa13a40c7e24c3e6
Internee <Internee@LRO-SH-21.vu.edu.pk>
2022-12-22 10:49:45 +0500

    Streamlit functions is added

--- helloWorld.py
+++ helloWorld.py
@@ -7,7 +7,13 @@ import plotly.express as pd
 import sklearn as sk
 import plotly as pt
 import seaborn as sns
+import tensorflow as tf
+import rasa as rs
 print("Hello World!!!")
+print(tf.__version__)
+print(rs.__version__)
 print("We are trying hard to make a ChatterBOT and Open Source to Eveyone :)")
-#This is just starting and yet to continue . . .
-# print("TensorFlow version:", tf.__version__)
+# This is just starting and yet to continue . . .
+print("TensorFlow version:", tf.__version__)
+st.header("Hello World!!!")
+st.write("Making ChatBOT . . .")
